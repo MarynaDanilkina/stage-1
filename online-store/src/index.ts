c727e8895b922e0f901a8af87290176531856efe
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-07-11 01:24:11 +0300

    feat: add Filters by value

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,3 +1,4 @@
+import { storage } from './storage';
 import { getData } from './getData';
 export { getDataNew };
 import './global.css';
@@ -18,12 +19,70 @@ interface IData {
     amount: number;
     size: string;
 }
+const BtnFilter = document.querySelectorAll('.sidebar-left__button');
 getData();
 
 function getDataNew(data: IData[]) {
+    BtnFilter.forEach((el) => el.addEventListener('click', (e) => AllMetod(e, data)));
     addDiv(data);
 }
+const div = <HTMLElement>document.querySelector('.product-container');
 
+function AllMetod(e: Event, data: IData[]) {
+    div.innerHTML = '';
+    const dataNew = data.slice();
+    const dataFilter = filter(e, dataNew);
+    addDiv(dataFilter);
+}
+function filter(event: Event, dataNew: IData[]) {
+    const firmArr: Array<string> = [];
+    const SeasonArr: Array<string> = [];
+    const ColorArr: Array<string> = [];
+    const SizeArr: Array<string> = [];
+    let PopularArr = '';
+    const GenderArr: Array<string> = [];
+    const target = <HTMLElement>event.target;
+    if (target.classList.contains('firm')) {
+        target.classList.toggle('active__button');
+    }
+    if (target.classList.contains('season')) {
+        target.classList.toggle('active__button');
+    }
+    if (target.classList.contains('color')) {
+        target.classList.toggle('active__button');
+    }
+    if (target.classList.contains('size')) {
+        target.classList.toggle('active__button');
+    }
+    if (target.classList.contains('popular')) {
+        target.classList.toggle('active__button');
+    }
+    if (target.classList.contains('gender')) {
+        target.classList.toggle('active__button');
+    }
+    document.querySelectorAll<HTMLElement>('.active__button').forEach((el) => {
+        if (typeof el.dataset.firm != 'undefined') {
+            firmArr.push(el.dataset.firm);
+        }
+        if (typeof el.dataset.season != 'undefined') {
+            SeasonArr.push(el.dataset.season);
+        }
+        if (typeof el.dataset.color != 'undefined') {
+            ColorArr.push(el.dataset.color);
+        }
+        if (typeof el.dataset.size != 'undefined') {
+            SizeArr.push(el.dataset.size);
+        }
+        if (typeof el.dataset.popular != 'undefined') {
+            PopularArr += el.dataset.popular;
+        }
+        if (typeof el.dataset.gender != 'undefined') {
+            GenderArr.push(el.dataset.gender);
+        }
+    });
+    const dataAll = storage.getDataFirm(firmArr, SeasonArr, ColorArr, SizeArr, PopularArr, GenderArr, dataNew);
+    return dataAll;
+}
 export function addDiv(data: IData[]) {
     for (let i = 0; i < data.length; i++) {
         const div = <HTMLElement>document.querySelector('.product-container');
--- online-store/src/storage.ts
+++ online-store/src/storage.ts
@@ -26,6 +26,87 @@ class Storage {
     getData = () => {
         return this.data;
     };
+    getDataFirm = (
+        value1: Array<string>,
+        value2: Array<string>,
+        value3: Array<string>,
+        value4: Array<string>,
+        value5: string,
+        value6: Array<string>,
+        dataNew: IData[]
+    ) => {
+        let result1: IData[] = [];
+        let result2: IData[] = [];
+        let result3: IData[] = [];
+        let result4: IData[] = [];
+        let result5: IData[] = [];
+        const result6: IData[] = [];
+        if (value1.length !== 0) {
+            for (let i = 0; i < value1.length; i++) {
+                dataNew.filter(function (user) {
+                    if (user.firm.indexOf(value1[i]) > -1) {
+                        result1.push(user);
+                    }
+                });
+            }
+        } else {
+            result1 = dataNew;
+        }
+        if (value2.length !== 0) {
+            for (let i = 0; i < value2.length; i++) {
+                result1.filter(function (user) {
+                    if (user.season.indexOf(value2[i]) > -1) {
+                        result2.push(user);
+                    }
+                });
+            }
+        } else {
+            result2 = result1;
+        }
+        if (value3.length !== 0) {
+            for (let i = 0; i < value3.length; i++) {
+                result2.filter(function (user) {
+                    if (user.color.indexOf(value3[i]) > -1) {
+                        result3.push(user);
+                    }
+                });
+            }
+        } else {
+            result3 = result2;
+        }
+        if (value4.length !== 0) {
+            for (let i = 0; i < value4.length; i++) {
+                result3.filter(function (user) {
+                    if (user.size.indexOf(value4[i]) > -1) {
+                        result4.push(user);
+                    }
+                });
+            }
+        } else {
+            result4 = result3;
+        }
+        if (value5.length !== 0) {
+            result4.filter(function (user) {
+                if (user.popular.indexOf(value5) > -1) {
+                    result5.push(user);
+                }
+            });
+        } else {
+            result5 = result4;
+        }
+        if (value6.length !== 0) {
+            for (let i = 0; i < value6.length; i++) {
+                result5.filter(function (user) {
+                    if (user.gender.indexOf(value6[i]) > -1) {
+                        result6.push(user);
+                    }
+                });
+            }
+        } else {
+            return result5;
+        }
+        return result6;
+    };
 }
 
 const storage = new Storage();
