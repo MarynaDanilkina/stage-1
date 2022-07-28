41f548d2f684902ec7a216987968fbac9aef0103
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-07-28 11:41:21 +0300

    feat: add car

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,3 +1,12 @@
 import './global.css';
 import { getPage } from './components/page';
+import { createCar } from './server/api';
 getPage();
+const newForm = document.getElementById('new-form') as HTMLDivElement;
+newForm.addEventListener('submit', () => newCar());
+
+async function newCar() {
+    const newName = document.getElementById('new-name') as HTMLInputElement;
+    const newColor = document.getElementById('new-color') as HTMLInputElement;
+    await createCar({ name: newName.value, color: newColor.value });
+}
--- async-race/src/server/api.ts
+++ async-race/src/server/api.ts
@@ -2,18 +2,30 @@ const url = 'http://127.0.0.1:3000';
 const path = {
     garage: '/garage',
 };
-export type Car = {
+export type Cars = {
     name: string;
     color: string;
     id: number;
 };
+export type Car = {
+    name: string;
+    color: string;
+};
 export async function getCars(page: number, limit = 7) {
     const res = await fetch(`${url}${path.garage}?_page=${page}&_limit=${limit}`);
-    const items: Car[] = await res.json();
+    const items: Cars[] = await res.json();
     const count = Number(res.headers.get('X-Total-Count'));
-    console.log(items);
-    console.log(count);
     return { items, count };
+}
+export async function createCar(car: Car) {
+    const res = await fetch(`${url}${path.garage}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(car),
+    });
+    return await res.json();
 }
 //export const main = async () => {
 //    const car = await getCars(1);
